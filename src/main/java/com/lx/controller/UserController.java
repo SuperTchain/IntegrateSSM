c310a8186c290230077a4a16393680bdd3146b4a
SuperTchain <allurelx@2980.com>
2020-04-22 12:37:24 +0800

    Login

--- src/main/java/com/lx/controller/UserController.java
+++ src/main/java/com/lx/controller/UserController.java
@@ -2,7 +2,9 @@ package com.lx.controller;
 
 import com.lx.model.User;
 import com.lx.service.UserService;
+import com.mysql.cj.Session;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
